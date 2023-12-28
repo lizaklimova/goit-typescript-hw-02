e562c81fceb1a97f2e198b1f117f7a07b9c3d602
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-12-28 20:43:27 +0200

    finished

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,33 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+const obj1: Pick<AllType, keyof AllType> = {
+  name: "Object 1",
+  position: 1,
+  color: "Red",
+  weight: 10,
+};
+
+const obj2: Pick<AllType, keyof AllType> = {
+  name: "Object 2",
+  position: 2,
+  color: "Blue",
+  weight: 20,
+};
+
+function compare<T extends AllType, K extends AllType>(
+  top: Pick<T, keyof AllType>,
+  bottom: Pick<K, keyof AllType>
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,12 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T, U> {
+  key: T;
+  value: U;
 }
+
+const pair1: KeyValuePair<string, boolean> = { key: "str", value: true };
+const pair2: KeyValuePair<string, number> = { key: "num", value: 5 };
 
 export {};
